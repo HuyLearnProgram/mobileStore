2b804a94a65033fef20d832ec28131706e030333
hungp03 <ph.hung0903@gmail.com>
2023-05-01 22:33:17 +0700

    update

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function getListProducts() {
 //Hàm khởi tạo cho tất cả các trang
 function init() {
   // Lấy data từ local storage
-  list_products = getListProducts() || list_products;
+  listProducts = getListProducts() || listProducts;
   adminInfo = getListAdmin() || adminInfo;
 
   setupEventAccount();
@@ -69,4 +69,34 @@ function animateCartNumber() {
   }, 1000);
 }
 
-function addToCart()
+function addToCart(masp, name){
+  let user  = getCurrentUser();
+  if (!user){
+    alert("Bạn cần đăng nhập để mua hàng!");
+    showAccount(true);
+    return;
+  }
+
+  if (user.off){
+    alert("Tài khoản bị khóa, không thể mua hàng!");
+    return;
+  }
+  let time = new Date();
+  let productExist = false;
+  for (let i = 0; i<user.products.length; i++){
+    //Kiểm tra xem có trùng sản phẩm trong giỏ hay không
+    if (user.products[i].ma == masp) {
+      user.products[i].qty++;
+      productExist = true;
+      break;
+    }
+  }
+  if (!productExist) {
+    // nếu không trùng thì mới thêm sản phẩm vào user.products
+    user.products.push({
+      ma: masp,
+      qty: 1,
+      date: time,
+    });
+}
+}
